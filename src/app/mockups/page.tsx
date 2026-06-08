6b88c5517b5ec28a61775ac8be762c5b8abd6b31
Aditya Baindur <adityabaindur@icloud.com>
2026-06-07 21:14:29 -0600

    added mockups and fixed deployment bug

--- src/app/mockups/page.tsx
+++ src/app/mockups/page.tsx
@@ -1,15 +1,7 @@
-import type { GetServerSideProps } from 'next'
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  return {
-    redirect: {
-      destination:
-        'https://www.figma.com/design/dDYRUaw96GKKY7U83yMDcJ/Untitled?node-id=0-1&t=2fttwBZD471Y3f3M-1',
-      statusCode: 307,
-    },
-  }
-}
+import { redirect } from 'next/navigation'
 
 export default function Mockups() {
-  return null
+  redirect(
+    'https://www.figma.com/design/dDYRUaw96GKKY7U83yMDcJ/Untitled?node-id=0-1&t=2fttwBZD471Y3f3M-1'
+  )
 }
